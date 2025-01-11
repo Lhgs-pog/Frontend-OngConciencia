c083c7fb558d57914b67db69b032f6761708f3fb
Daniel Umberto <danielumberto51@gmail.com>
2025-01-11 16:28:23 -0300

    :sparkles: feat: Página de Entrar e Cadastrar

---
+++ LoginCadastro/script.js
@@ -0,0 +1,78 @@
+const loginBtn = document.querySelector("#login");
+const registerBtn = document.querySelector("#register");
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+
+
+/* Mostrar o formulário de entrar */
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "#21264D";
+    registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+    loginForm.style.left = "50%";
+    registerForm.style.left = "-50%";
+
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+})
+
+/* Mostrar o formulário de cadastrar */
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    registerBtn.style.backgroundColor =  "#21264D";
+
+    loginForm.style.left = "150%";
+    registerForm.style.left = "50%";
+
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 1;
+})
+
+/* Mostrar senha */
+const logInputField = document.getElementById('logPassword');
+const logInputIcon = document.getElementById('log-pass-icon');
+
+const regInputField = document.getElementById('regPassword');
+const regInputIcon = document.getElementById('reg-pass-icon');
+
+/* Mostrar senha no Entrar */
+function myLogPassword(){
+    if(logInputField.type === "password"){
+        logInputField.type = "text";
+
+        logInputIcon.name = "eye-off-outline";
+        logInputIcone.style.cursor = "pointer";
+    }else{
+        logInputField.type = "password";
+
+        logInputIcon.name = "eye-outline";
+        logInputIcone.style.cursor = "pointer";
+    }
+}
+
+/* Mostrar senha no Cadastrar */
+function myRegPassword(){
+    if(regInputField.type === "password"){
+        regInputField.type = "text";
+
+        regInputIcon.name = "eye-off-outline";
+        regInputIcone.style.cursor = "pointer";
+    }else{
+        regInputField.type = "password";
+
+        regInputIcon.name = "eye-outline";
+        regInputIcone.style.cursor = "pointer";
+    }
+}
+
+
+/* Mudar o ícone quando o usuário digitar a senha */
+function changeIcon(value){
+    if(value.length > 0){
+        logInputIcon.name = "eye-outline";
+        regInputIcon.name = "eye-outline";
+    }else{
+        logInputIcon.name = "lock-closed-outline"
+        regInputIcon.name = "lock-closed-outline"
+    }
+}
