935be8dc8c8214dd2418b17868931453c8bb4658
Lucas Henrique Gonçalves Souto <lucashenriquegoncalvessouto1@gmail.com>
2025-02-02 08:16:48 -0300

    Criação da conexão entre o back e front da página da ong, ainda não foi testada pq eu preciso do uuid na url primeiro

---
+++ PaginaONG/script.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Recupera os elementos no html
+    let imagem = document.getElementById('imagem-ong');
+    let nome = document.getElementById('nome');
+    let descricao = document.getElementById('descricao');
+    let email = document.getElementById('email');
+    let telefone = document.getElementById('telefone');
+    let site = document.getElementById('site');
+    
+    /*
+    * Pega o uuid passado pela página home
+    */
+    function pegarUUID(){
+        const params = new URLSearchParams(window.location.search);
+        return params.get("uuid");
+    }
+
+    /*
+    * Função fetch para recuperar os dados da ong e colocalos no html
+    */
+    async function DadosOng() {
+        const url = `http://localhost:8080/ong/${id}`;
+        const uuid = pegarUUID();
+
+        try{
+            //Configuração e chamada do fetch
+            let resposta = await fetch( url, {
+                method: 'GET',
+                headers: {
+                    "Content-Type":"application/json",
+                },
+            });
+    
+            //Verifica se ocorreu algum erro e passa ele no console
+            if(!resposta.ok){
+                console.error(`Erro ao coletar os dados da Ong. Status da requisição: ${resposta.status}, Conteudo: ${resposta.body}`);
+                throw new Error(`Erro ao coletar os dados da Ong. Status da requisição: ${resposta.status}, Conteudo: ${resposta.body}`);
+            }
+    
+            //Converte a resposta json em uma variavel
+            let dadosOng = await resposta.json();
+    
+            //Modifica os valores no html pelos os obtidos na ong
+            imagem.src = dadosOng.imagem;
+            nome.textContent = dadosOng.nome;
+            descricao.textContent = dadosOng.descricao;
+            email.textContent = dadosOng.email;
+            telefone.textContent = dadosOng.telefone;
+            site.href = dadosOng.link;
+            
+        }catch(error){
+            console.error(`Não foi possível recuperar os dados da Ong. Error: ${error}`);
+        }      
+    }
+
+    //Chama a função para pegar os dados da ong assim que a página carregar
+    DadosOng();
+})
